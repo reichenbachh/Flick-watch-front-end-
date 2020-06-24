b01409542cf0e34295a2e5ba089520f34dba324a
George Andong <Georgeansong7@gmail.com>
2020-06-24 23:07:17 +0000

    Home page setUp

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,42 @@
+import React from "react";
+import LOGOw from "../../assets/LOGOw.png";
+
+const Nav = () => {
+  return (
+    <div id='home-landing'>
+      <div className='nav'>
+        <div className='logo'>
+          <a href='#!'>
+            <img src={LOGOw} alt='' srcset='' />
+          </a>
+        </div>
+        <div className='menu'>
+          <ul>
+            <li>
+              <a href='#!'>Discover</a>
+            </li>
+            <li>
+              <a href='#!'>Tv Shows</a>
+            </li>
+            <li>
+              <a href='#!'>Movies</a>
+            </li>
+            <li>
+              <a href='#!'>Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='landing-text'>
+        <h1>
+          <span className='disco-link'>
+            <a href=''>Discover </a>
+          </span>{" "}
+          and track your favourite movies and TV Shows
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
